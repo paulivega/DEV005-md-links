f261c810f4c9f2eb7ff96b5c49618b6ae9dadc44
Paulinavegac <paulina.vegac@gmail.com>
2023-05-17 17:18:35 -0400

    CREATE: folder and UPDATE mdlinks execution

---
+++ src/cli.js
@@ -0,0 +1,14 @@
+const { mdLinks } = require('./index.js');
+// argumento de mdLinks
+const userPath = process.argv[2];
+
+// Para consumir la función md link, primero ejecutar y pasar el argumento.
+mdLinks(userPath)
+//luego van las acciones del then y el catch
+.then((result) => {
+  console.log('saliendo por el then', result);
+})
+.catch((error) => {
+  console.log('saliendo por el catch:', error);
+  process.exit()
+});
