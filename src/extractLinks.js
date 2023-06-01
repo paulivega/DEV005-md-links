42c278bf41399f2e281c1545abd3434e3d57f974
Paulinavegac <paulina.vegac@gmail.com>
2023-06-01 17:34:03 -0400

    UPDATE: function extract links

--- src/extractLinks.js
+++ src/extractLinks.js
@@ -1,14 +1,11 @@
-const { JSDOM } = require("jsdom");
+const { JSDOM } = require('jsdom');
 const fsModules = require('fs');
 const { marked } = require('marked');
-const arrayFilesMdPrueba = [
-  '/Users/pauli/laboratoria/DEV005-md-links/src/files/first.md',
-  '/Users/pauli/laboratoria/DEV005-md-links/src/files/moreFiles/third.md',
-  '/Users/pauli/laboratoria/DEV005-md-links/src/files/second.md'
-];
 // fn convierte contenido .md a html y extraer los elementos anchor 
+const text = 'El segundo link, es la página de [laboratoria](https://www.laboratoria.la/)lugar al que nunca más entré desde que abrí mi cuenta para postular.';
+
 const convertMdtoHtml = (file) => {
-  // pasar .md a html usando la librería marked
+  // pasar .md a html usando la librería markedn retorna un htmlColecction
   const htmlText = marked(file,
     {
       headerIds: false,
@@ -16,44 +13,44 @@ const convertMdtoHtml = (file) => {
     },);
   // extraer los anchors usando JSDOM
   const dom = new JSDOM(htmlText);
-  const elementsAnchor = dom.window.document.getElementsByTagName('a')
-  return elementsAnchor
-}
+  const elementsAnchor = dom.window.document.getElementsByTagName('a');
+  return elementsAnchor;
+};
 // fn obtiene los links desde HTMLCollection
-const getLinksFromHtml = (elementsAnchor, fileRoute) => {
+  const getLinksFromHtml = (elementsAnchor, fileRoute) => {
   // element Anchor(HTMLCollection) se transforma array
   const arrayAnchor = Array.from(elementsAnchor);
   //recorrer el arreglo y me entrega el :href y .text de cada anchor
   const anchor = arrayAnchor.map((anchor) => ({
     href: anchor.href,
-    text: anchor.textContent,
+    text: anchor.textContent.substring(0,50),
     file: fileRoute,
   }));
-  return anchor
+  return anchor;
 };
-
 //Declaración de la promesa que leer archivo.md
 const readMdFile = (fileRoute) => {
   return new Promise((resolve, reject) => {
     fsModules.readFile(fileRoute, 'utf-8', (err, file) => {
       if (err) {
-        reject(err)
+        reject(err);
       } else {
         const convertToHtml = convertMdtoHtml(file);
         const getLinks = getLinksFromHtml(convertToHtml, fileRoute);
-        resolve(getLinks)
-        
+        resolve(getLinks);
       }
-    })
-  })
+    });
+  });
 };
-
 // recorrer arreglo ArrayFilesMD y ejecutar readMdFile a cada archivo usando .map
 const getAllLinks = (arrayFilesMd) => {
   const arrayLinks = arrayFilesMd.map(file => {
-    return readMdFile(file)
+    return readMdFile(file);
   });
   return Promise.all(arrayLinks);
-}
-module.exports = { getAllLinks, };
+};
+module.exports = { getAllLinks, convertMdtoHtml, getLinksFromHtml, readMdFile };
+const testPath = '/Users/pauli/laboratoria/DEV005-md-links/src/files'
 
+//console.log(getLinksFromHtml(convertMdtoHtml(text), testPath) )
+//node src/extractLinks.js
